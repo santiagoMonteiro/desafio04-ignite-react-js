48711a25bdf6d754764e4e018c1b26edea954fe9
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2021-09-16 10:16:45 -0300

    partial migration to typescript and tsx

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,7 +9,12 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps {
+  name: string;
+  placeholder: string;
+}
+
+function Input({ name, icon: Icon, ...rest }: InputProps) {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
